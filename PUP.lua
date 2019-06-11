271358a02a4b583058b0e0c2b1f53ffc38f111bb
Anthony Perry <atperry7@gmail.com>
2019-06-10 21:18:48 -0500

    Missing brackets for Shinjin Spiral Set

--- PUP.lua
+++ PUP.lua
@@ -356,7 +356,7 @@ function init_gear_sets()
     sets.precast.WS["Shijin Spiral"] =
         set_combine(
         sets.precast.WS,
-        -- Add your set here 
+        { -- Add your set here }
     )
 
     sets.precast.WS["Howling Fist"] = set_combine(sets.precast.WS, {})
